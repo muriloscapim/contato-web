abaad6ce0e2d992dcd1633401472aca4bfa9fa49
Murilo Scapim <murilo.scapim@outlook.com>
2020-05-25 22:44:07 -0300

    Primeiro commit

---
+++ ContatoWeb/ContatoWeb/Models/AbstractRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace ContatoWeb.Models
+{
+    public abstract class AbstractRepository<TEntity, TKey>
+        where TEntity : class
+    {
+        // obtem do arquivo web.config a string de conexão com o banco
+        protected string StringConn { get; } = WebConfigurationManager.
+            ConnectionStrings["Databasecrud"].ConnectionString;
+
+        public abstract List<TEntity> GetAll();
+        public abstract TEntity GetById(TKey id);
+        public abstract void Save(TEntity entity);
+        public abstract void Update(TEntity entity);
+        public abstract void Delete(TEntity entity);
+        public abstract void DeleteById(TKey id);
+    }
+}
---
+++ ContatoWeb/ContatoWeb/Models/ContatoRepository.cs
@@ -0,0 +1,49 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContatoWeb.Models
+{
+    public class ContatoRepository : AbstractRepository<Contato, int>
+    {
+        public override void Delete(Contato entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DeleteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<Contato> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Contato GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Save(Contato entity)
+        {
+            using (var conn = new NpgsqlConnection(StringConn))
+            {
+                string sql = "INSERT INTO tb_contato (nome, email, telefone)" +
+                             "VALUES (@nome, @email, @telefone);";
+                NpgsqlCommand cmd = new NpgsqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@nome", entity.Nome);
+                cmd.Parameters.AddWithValue("@email", entity.Email);
+                cmd.Parameters.AddWithValue("@telefone", entity.Telefone);
+            }
+        }
+
+        public override void Update(Contato entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
